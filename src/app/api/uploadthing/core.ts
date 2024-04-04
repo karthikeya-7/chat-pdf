da349b97a12a66ca27a7f8c31361ef58733e9ca7
karthikeya-7 <pydikarthikeya@gmail.com>
2024-04-04 16:45:07 +0530

    Chat component

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,7 +3,10 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { UTApi } from "uploadthing/server";
- 
+import {PDFLoader} from "langchain/document_loaders/fs/pdf"
+import {OpenAIEmbeddings} from  'langchain/embeddings/openai'
+import {getPineconeClient} from "@/lib/pinecone"
+import {PineconeStore} from "@langchain/pinecone"
 export const utapi = new UTApi();
  
  
@@ -33,7 +36,61 @@ export const ourFileRouter = {
 
         },
       })
-    }),
+
+      try {
+        const response = await fetch (file.url)
+        const blob = await response.blob()
+
+        const loader = new PDFLoader(blob)
+
+        const pageLevelDocs = await loader.load()
+
+        const pagesAmt = pageLevelDocs.length
+
+
+
+        //vectorize and index the entire document
+        const pinecone = await getPineconeClient()
+        const pineconeIndex = pinecone.Index("chat-pdf")
+
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY
+        })
+
+
+        await PineconeStore.fromDocuments(
+          pageLevelDocs,
+          embeddings,
+          {
+            //@ts-ignore
+            pineconeIndex,
+            namespace: createdFile.id,
+          }
+        )
+        
+        await db.file.update({
+          data: {
+            uploadStatus: 'SUCCESS',
+          },
+          where: {
+            id: createdFile.id,
+          },
+        })
+
+      } catch (err:any) {
+        console.log(err.message)
+        await db.file.update({
+          data: {
+            uploadStatus: 'FAILED',
+          },
+          where: {
+            id: createdFile.id,
+          },
+        })
+      }
+    }
+
+    )
 } satisfies FileRouter;
  
 export type OurFileRouter = typeof ourFileRouter;
---
+++ src/lib/pinecone.ts
@@ -0,0 +1,12 @@
+import { Pinecone } from '@pinecone-database/pinecone'
+
+export const getPineconeClient = async () => {
+  const client = new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY!,
+    environment: 'gcp-starter',
+  })
+
+
+
+  return client
+}
