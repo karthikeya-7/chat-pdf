471db73079ccf209f01742208c2392fe83297156
karthikeya-7 <pydikarthikeya@gmail.com>
2024-04-06 00:05:56 +0530

    stripe update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,48 +1,84 @@
-import MaxWidthWrapper from "./MaxWidthWrapper"
-import Link from "next/link"
-import { buttonVariants } from "./ui/button"
+import Link from 'next/link'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import { buttonVariants } from './ui/button'
 import {
-    LoginLink,
-    RegisterLink,
-    getKindeServerSession,
-  } from '@kinde-oss/kinde-auth-nextjs/server'
-const Navbar =() => {
-    return(
-        <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
-            <MaxWidthWrapper>
-                <div className="flex h-14 items-center justify-between border-b border-zinc-200">
-                    <Link href='/' className="flex- z-40 font-semibold">
-                    <span>Chat-PDF</span>
-                    </Link>
+  LoginLink,
+  RegisterLink,
+  getKindeServerSession,
+} from '@kinde-oss/kinde-auth-nextjs/server'
+import { ArrowRight } from 'lucide-react'
+import UserAccountNav from './UserAccountNav'
+import MobileNav from './MobileNav'
 
+const Navbar = async () => {
+  const { getUser } = await getKindeServerSession()
+  const user = await getUser()
 
-                    <div className="hidden items-center space-x-4 sm:flex">
-                        <>
-                            <Link
-                            href="/pricing"
-                            className={buttonVariants({
-                                variant: 'ghost',
-                                size: 'sm',
+  return (
+    <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+      <MaxWidthWrapper>
+        <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
+          <Link
+            href='/'
+            className='flex z-40 font-semibold'>
+            <span>quill.</span>
+          </Link>
 
-                            })}> Pricing </Link>
+          <MobileNav isAuth={!!user} />
 
-                            <LoginLink className={buttonVariants({
-                                variant: 'ghost',
-                                size: 'sm',
-                            })}> Sign In
-                            </LoginLink>
+          <div className='hidden items-center space-x-4 sm:flex'>
+            {!user ? (
+              <>
+                <Link
+                  href='/pricing'
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}>
+                  Pricing
+                </Link>
+                <LoginLink
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}>
+                  Sign in
+                </LoginLink>
+                <RegisterLink
+                  className={buttonVariants({
+                    size: 'sm',
+                  })}>
+                  Get started{' '}
+                  <ArrowRight className='ml-1.5 h-5 w-5' />
+                </RegisterLink>
+              </>
+            ) : (
+              <>
+                <Link
+                  href='/dashboard'
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}>
+                  Dashboard
+                </Link>
 
-                            <RegisterLink className={buttonVariants({
-                                size: 'sm',
-                            })}> Register
-                            </RegisterLink>
-                        </>
-
-                    </div>
-                </div>
-            </MaxWidthWrapper>
-        </nav>
-    )
+                <UserAccountNav
+                  name={
+                    !user.given_name || !user.family_name
+                      ? 'Your Account'
+                      : `${user.given_name} ${user.family_name}`
+                  }
+                  email={user.email ?? ''}
+                  imageUrl={user.picture ?? ''}
+                />
+              </>
+            )}
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  )
 }
 
 export default Navbar
